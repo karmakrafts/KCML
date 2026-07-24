18696fd093dc5c3e5407d7f80ddf8d372f153595
KitsuneAlex <alexh@karmakrafts.dev>
2026-07-24 17:33:33 +0200

    Add Kotlin Multiplatform dependency to root buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ import dev.karmakrafts.conventions.signPublications
 plugins {
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.shadow) apply false
